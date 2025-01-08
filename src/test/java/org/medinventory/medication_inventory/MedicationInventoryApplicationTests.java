4b885b8057d9649f258c3a5f752c10e33c395c8a
ratio <ratio_dd@163.com>
2025-01-09 01:53:46 +0800

    ReCreate project with SpringBoot

---
+++ src/test/java/org/medinventory/medication_inventory/MedicationInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.medinventory.medication_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
