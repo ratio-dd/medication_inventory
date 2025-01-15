7466ad4762dba3aed093423e2f09115a50a74f52
ratio <ratio_dd@163.com>
2025-01-15 13:00:01 +0800

    config: add CORS configuration to allow cross-origin requests
    - Create CorsConfig class to configure CORS settings
    - Allow all origins, methods, and headers for all mappings
    - Expose Access-Control-Allow-Origin header

--- src/main/java/org/medinventory/medication_inventory/MedicationInventoryApplication.java
+++ src/main/java/org/medinventory/medication_inventory/MedicationInventoryApplication.java
@@ -1,9 +1,12 @@
 package org.medinventory.medication_inventory;
 
+import org.medinventory.medication_inventory.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class MedicationInventoryApplication {
 
     public static void main(String[] args) {
