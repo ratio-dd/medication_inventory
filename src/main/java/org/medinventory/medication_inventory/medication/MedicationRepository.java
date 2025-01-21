fc27e67d334ff8150b3e585d3f14af56efdd73e8
ratio <ratio_dd@163.com>
2025-01-21 10:15:54 +0800

    refactor(medication): remove unused method from MedicationRepository
    - Removed findByCommonName method from MedicationRepository interface
    - This method was likely unused, as it is not utilized in the current codebase

--- src/main/java/org/medinventory/medication_inventory/medication/MedicationRepository.java
+++ src/main/java/org/medinventory/medication_inventory/medication/MedicationRepository.java
@@ -3,9 +3,7 @@ package org.medinventory.medication_inventory.medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 
 public interface MedicationRepository
         extends JpaRepository<Medication, Integer>, JpaSpecificationExecutor<Medication> {
-    List<Medication> findByCommonName(String name);
 }
